aba42ec7f3de20ee5a888702bb8f62abc859b008
ThanhHuyen2212 <huyennguyen850.vn@gmail.com>
2023-03-13 20:10:40 +0700

    base code

---
+++ src/main/java/Main/MainApp.java
@@ -0,0 +1,11 @@
+package Main;
+
+import javafx.application.Application;
+import javafx.stage.Stage;
+
+public class MainApp extends Application {
+    @Override
+    public void start(Stage stage) throws Exception {
+
+    }
+}
