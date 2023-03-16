2a40ad4ae15013cedd2e3ee97b2fd7717d7f0741
ThanhHuyen2212 <huyennguyen850.vn@gmail.com>
2023-03-16 23:13:07 +0700

    Revert "Huyen fix code Depot management"
    This reverts commit c89864120b5ee2af2434ea9b06cd24e26090244f.

--- src/main/java/App/Depot/Controller/DetailRecipeController.java
+++ src/main/java/App/Depot/Controller/DetailRecipeController.java
@@ -5,7 +5,6 @@ import javafx.scene.control.Button;
 import javafx.scene.control.TableColumn;
 import javafx.scene.control.TableView;
 import javafx.scene.control.TextField;
-import javafx.scene.control.Label;
 
 import java.awt.*;
 
