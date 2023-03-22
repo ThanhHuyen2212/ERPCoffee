37175a661987ade15bd08d683b49279d70ed648e
daisgu014 <daibucky@gmail.com>
2023-03-23 02:26:14 +0700

    Code customer

---
+++ src/main/java/App/Controller/CustomerAdd.java
@@ -0,0 +1,32 @@
+package App.Controller;
+
+import javafx.collections.FXCollections;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.control.ButtonType;
+import javafx.scene.control.Dialog;
+import javafx.scene.control.DialogPane;
+import javafx.scene.control.TextField;
+import javafx.scene.layout.Pane;
+import javafx.stage.StageStyle;
+
+import java.io.IOException;
+import java.sql.SQLException;
+import java.util.Optional;
+
+public class CustomerAdd {
+    @FXML
+    private DialogPane dialogCategoryAdd;
+    @FXML
+    private TextField txtName;
+
+    @FXML
+    private TextField txtPhone;
+
+    @FXML
+    private TextField txtPoint;
+
+
+
+}
