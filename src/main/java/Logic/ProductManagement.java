d7fd6b4578159f008e8a2bba29917b57400b937e
kudoshinichi260 <kudoshinichi260@gmail.com>
2023-03-29 23:25:07 +0700

    Fix code GiaDai

--- src/main/java/Logic/ProductManagement.java
+++ src/main/java/Logic/ProductManagement.java
@@ -10,13 +10,9 @@ import java.util.ArrayList;
 
 public class ProductManagement {
     private ArrayList<Product> products;
-<<<<<<< HEAD
-    ProductAccess productAccess = new ProductAccess();
-=======
+
     ProductAccess productAccess;
 
-
->>>>>>> main
     public ProductManagement(){
         productAccess = new ProductAccess();
         products=productAccess.retrieve();
